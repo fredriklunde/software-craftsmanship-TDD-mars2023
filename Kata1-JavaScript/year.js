942dcbaa09409e31446f456861a48bcb7a83f59b
Fredrik Lunde <fredrik.lunde3@gmail.com>
2023-03-30 11:39:03 +0200

    as implemented by GPT

--- Kata1-JavaScript/year.js
+++ Kata1-JavaScript/year.js
@@ -1,13 +1,13 @@
-const isLeap = (year) => {
-  if(year % 400 === 0 || (year % 4 === 0 && year % 100 != 0)){
-    if((year % 100 === 0 && year % 400 != 0) || year % 4 != 0){
-      return false;
-    }
-  }
-  else{
+function isLeap(year) {
+  if (year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
     return false;
   }
-  return true
-};
+}
 
 module.exports = isLeap;
--- Kata1-JavaScript/year.test.js
+++ Kata1-JavaScript/year.test.js
@@ -1,21 +1,26 @@
-const isLeap = require("./year");
+const isLeap = require('./year.js');
 
-test("All years divisible by 400 Are Leap Years", () => {
-  expect(isLeap(2000)).toBe(true);
-});
+describe('isLeap', () => {
+  test('All years divisible by 400 are leap years', () => {
+    expect(isLeap(2000)).toBe(true);
+  });
 
-test("All years not divisible by 400 Are NOT Leap Years", () => {
-  expect(isLeap(2001)).toBe(false);
-});
+  test('All years divisible by 100 but not by 400 are NOT leap years', () => {
+    expect(isLeap(1700)).toBe(false);
+    expect(isLeap(1800)).toBe(false);
+    expect(isLeap(1900)).toBe(false);
+    expect(isLeap(2100)).toBe(false);
+  });
 
-test("All years divisible by 100 but not 400 are NOT leap years", () => {
-  expect(isLeap(1700)).toBe(false);
-});
+  test('All years divisible by 4 but not by 100 are leap years', () => {
+    expect(isLeap(2008)).toBe(true);
+    expect(isLeap(2012)).toBe(true);
+    expect(isLeap(2016)).toBe(true);
+  });
 
-test("All years divisible by 4 but not 100 are leap years", () => {
-  expect(isLeap(2008)).toBe(true);
-});
-
-test("All years not divisible by 4 are NOT leap years", () => {
-  expect(isLeap(2017)).toBe(false);
+  test('All years not divisible by 4 are NOT leap years', () => {
+    expect(isLeap(2017)).toBe(false);
+    expect(isLeap(2018)).toBe(false);
+    expect(isLeap(2019)).toBe(false);
+  });
 });
